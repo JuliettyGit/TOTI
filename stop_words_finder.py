9b85e5cea837f52aa82be660bbdffa8446d23b40
Yuliia <laptievajulia@gmail.com>
2022-05-24 18:04:02 +0300

    improved stop_words_finder

--- stop_words_finder.py
+++ stop_words_finder.py
@@ -8,19 +8,25 @@ def find_stopwords_with_nltk(text_to_analise):
     stop_words = set(stopwords.words('english'))
     words = tokenization_with_nltk(text_to_analise)
     words_filtered = []
+    new_text = []
     for w in words:
         if w not in stop_words:
+            new_text.append(w)
+        if w in stop_words:
             words_filtered.append(w)
 
-    return "English text without stopwords from NLTK: ", words_filtered
+    return "English text without stopwords from NLTK: ", new_text, "Removed words: ", words_filtered
 
 
 def find_ukrainian_stopwords(text_to_analise):
     stopwords = text_creator(stop_words)
     words = tokenization_with_pymorphy(text_to_analise)
     words_filtered = []
+    new_text = []
     for w in words:
         if w not in stopwords:
+            new_text.append(w)
+        if w in stopwords:
             words_filtered.append(w)
 
-    return "Ukrainian text without stopwords: ", words_filtered
+    return "Ukrainian text without stopwords: ", new_text, "Removed words: ", words_filtered
